7833d7d60133363851a8d1febc65c2601d58ca2e
Fabricio Quintana <fabriciorcq@gmail.com>
2026-08-03 16:58:37 -0300

    test: corrigindo erros dos testes automatizados para o clean package

--- src/test/java/com/marmitech/Marmitech/Controller/CategoriaControllerTest.java
+++ src/test/java/com/marmitech/Marmitech/Controller/CategoriaControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -29,6 +30,7 @@ import static org.mockito.ArgumentMatchers.eq;
 
 
 @WebMvcTest(CategoriaController.class)
+@AutoConfigureMockMvc(addFilters = false)
 @ActiveProfiles("test")
 class CategoriaControllerTest {
 
