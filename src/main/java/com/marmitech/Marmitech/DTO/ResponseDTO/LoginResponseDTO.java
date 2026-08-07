5ed5f8ce18415cc34ed74e950e9937f003ab26a9
heron <herongamer0610@gmail.com>
2026-08-07 00:16:14 -0300

    Revert "Merge pull request #5 from devheron/feature/criacao-JWT"
    This reverts commit 07333bf0056b24d78e41d994cb863131aad537fa, reversing
    changes made to ce1aacb64236959b15588380415550aef6b3d2e3.

--- src/main/java/com/marmitech/Marmitech/DTO/ResponseDTO/LoginResponseDTO.java
+++
@@ -1,9 +0,0 @@
-package com.marmitech.Marmitech.DTO.ResponseDTO;
-
-public record LoginResponseDTO(
-        String token,
-        Integer id,
-        String nome,
-        String email,
-        String cargo
-) {}
